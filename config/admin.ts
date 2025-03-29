0536066e7cb37267019a64069777bdac00485b2d
Ash Hendricks <iplaywhatilike@gmail.com>
2025-03-29 15:06:12 +0200

    Add transfer token salt for data migration

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,38 @@
+// export default ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+// });
+
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env(
+        "TRANSFER_TOKEN_SALT",
+        "dj83hf8jd92hf63jd73he83h2d81h3df74jh2f74jd2"
+      ),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
